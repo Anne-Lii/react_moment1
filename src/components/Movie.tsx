87e0937f3a0f16e79e88475d9b12e05d60b02e49
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-23 19:27:00 +0100

    justerat kommentarer och ändrat key från index till id och lagt till id på listan med filmer

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import './Movie.css'
+import './Movie.css' //Importerar komponentspecifik css
 
+//Definierar ett gränssnitt för komponentens props
 interface MovieProps {
-    title: string;
-    year: number;
-    seen: boolean;
+    title: string; //filmens titel
+    year: number;//utgivningsår
+    seen: boolean;//Om jag har sett filmen eller ej
 }
 
+//funktionell komponent som representerar en enskild film
 const Movie: React.FC<MovieProps> = ({ title, year, seen }) => {
     return (
-      <div className='oneMovie'>
-        <h3>{title}</h3>
-        <h4>({year})</h4>
-        <p>Sett: {seen ? "Ja" : "Nej "}</p>
-        <img className='default_picture' src="src/assets/movie.png" alt="" /> {/* BIld av <a href="https://pixabay.com/sv/users/openclipart-vectors-30363/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=147631">OpenClipart-Vectors</a> från <a href="https://pixabay.com/sv//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=147631">Pixabay</a> */}
-        
+      <div className='oneMovie'>{/* omslutande div för att kunna skicka tillbaka fler element */}
+        <h3>{title}</h3> {/* Filmens titel */}
+        <h4>({year})</h4> {/* Utgivningsår inom parentes */}
+        <p>Sett: {seen ? "Ja" : "Nej "}</p> {/* Har filmen setts eller ej */}
+
+        {/* Standardbild. här kan url för bild läggas in eller statiska bilder */}
+        <img className='default_picture' src="src/assets/movie.png" alt="" />
+        {/* Bild av <a href="https://pixabay.com/sv/users/openclipart-vectors-30363/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=147631">OpenClipart-Vectors</a> från <a href="https://pixabay.com/sv//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=147631">Pixabay</a> */}
+         
       </div>
     );
 };
 
-export default Movie;
+export default Movie;//Export av komponenten så den går att använda på flera ställen
 
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import Movie from './Movie';
+import Movie from './Movie';//import av komponenten Movie 
 import './MovieList.css';
 
+//Funktionell komponent som visar en lista med filmer
 const MovieList: React.FC = () => {
 
   // En array med filmer
   const movies = [
-    { title: "The Goonies", year: 1985, seen: true },
-    { title: "The Notebook", year: 2004, seen: true },
-    { title: "Pulp Fiction", year: 1994, seen: false },
-    { title: " Intouchables", year: 2011, seen: true },
-    { title: "Braveheart", year: 1995, seen: true },
-    { title: "Master and Commander", year: 2003, seen: true },
+    { id: 1, title: "The Goonies", year: 1985, seen: true },
+    { id: 2, title: "The Notebook", year: 2004, seen: true },
+    { id: 3, title: "Pulp Fiction", year: 1994, seen: false },
+    { id: 4, title: " Intouchables", year: 2011, seen: true },
+    { id: 5, title: "Braveheart", year: 1995, seen: true },
+    { id: 6, title: "Master and Commander", year: 2003, seen: true },
   ];
 
   return (
     <div className='main_content'>
       <h2>Favoriter</h2>
       <div className='movie_content'>
-      {movies.map((movie, index) => (
-        <Movie key={index} title={movie.title} year={movie.year} seen={movie.seen} />
+
+      {/* Itererar över movies-arrayen och skapar en Movie-komponent för varje objekt */}
+      {movies.map((movie) => (
+        
+        //Skickar props
+        <Movie 
+        key={movie.id} 
+        title={movie.title} 
+        year={movie.year} 
+        seen={movie.seen}
+         />
       ))}
       </div>
     </div>
